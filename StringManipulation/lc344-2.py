3b78893eaae9accd1462ac45ecd799968c1dd63f
EdwardYoungMoon <jmoon0227@gmail.com>
2022-07-21 16:46:26 +0900

    leetcode 344

---
+++ StringManipulation/lc344-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
+            
+    
+a = Solution()
+print(a.reverseString(["h","e","l","l","o"]))
