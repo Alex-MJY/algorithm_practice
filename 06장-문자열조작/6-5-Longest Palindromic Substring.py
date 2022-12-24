3337f2b55191ab270df69fb19bca9cf999f60cf9
EdwardYoungMoon <jmoon0227@gmail.com>
2022-12-25 00:12:45 +0900

    modified 49, 5, 1, 42

--- 06장-문자열조작/6-5-Longest Palindromic Substring.py
+++ 06장-문자열조작/6-5-Longest Palindromic Substring.py
@@ -6,7 +6,7 @@ class Solution:
                 right += 1
             return s[left + 1 : right - 1]
         
-        if len(s) < 2 or s == s[::-1]:
+        if len(s) < 2 or s == s[::-1]: # 예외처리
             return s
         
         result = ''
@@ -14,3 +14,12 @@ class Solution:
         for i in range(len(s) - 1):
             result = max(result, expand(i, i+1), expand(i, i+2), key=len)
         return result
+    
+    
+'''
+2칸, 3칸으로 구성된 투 포인터가 슬라이딩 윈도우처럼 전진해 나간다.
+이때 윈도우에 들어온 문자열이 팰린드롬인 경우 그 자리에 멈추고, 투 포인터가 점점 확장하는 식이다.
+팰린드롬은 짝수일 때도 있고, 홀수일 때도 있다. 
+
+함수가 발산하여 무한 루프에 빠지지 않도록 예외처리를 잊으면 안된다.
+'''
--- 07장-배열/1-1-Two Sum-3.py
+++ 07장-배열/1-1-Two Sum-3.py
@@ -9,3 +9,12 @@ class Solution:
         for i, num in enumerate(nums):
             if target - num in nums_map and i != nums_map[target - num]:
                 return nums.index(num), nums_map[target - num]
+            
+'''
+타겟에서 첫 번째 수를 빼면 두 번째 수를 알아낼 수 있다.
+두 번째 수를 키로 하고 기존의 인덱스는 값으로 바꿔서 딕셔너리에 저장하면, 나중에
+두 번째 수를 키로 조회해서 정답을 찾아낼 수 있다.
+
+타겟에서 첫 번째 수를 뺀 결과를 키로 조회하면 두 번째 수의 인덱스를 O(1)에 조회할 수 있다.
+최악의 경우는 O(n)이지만 매우 드물다.
+'''
