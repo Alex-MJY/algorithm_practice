b793f020a031db998cd973fc6ea6aeaba503be6b
Alex-MJY <jmoon0227@gmail.com>
2023-03-30 14:29:32 +0900

    leetcode 122, 406

---
+++ python-algorithm-interview/21장 그리디 알고리즘/1-122-Best Time to Buy and Sell Stock II-1.py
@@ -0,0 +1,9 @@
+# greedy
+class Solution:
+    def maxProfit(self, prices) -> int:
+        result = 0
+        # 값이 오르는 경우 매번 그리디 계산
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                result += prices[i+1] - prices[i]
+        return result
