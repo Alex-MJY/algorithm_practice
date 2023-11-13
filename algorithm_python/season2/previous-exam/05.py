63513cdf1e0a22f2d21e21652c99818ac42b4119
Alex-MJY <jmoon0227@gmail.com>
2023-11-13 16:21:52 +0900

    algorithm previous-exam 06,07,08

--- algorithm_python/season2/previous-exam/05.py
+++ algorithm_python/season2/previous-exam/05.py
@@ -1,5 +1,6 @@
 # 볼링공 고르기 (315p, 512p)
 
+# 1
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
 result = 0
@@ -8,4 +9,19 @@ for i in range(n):
     for j in range(i + 1, len(data)):
         if data[i] != data[j]:
             result += 1
+print(result)
+
+
+# 2
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+array = [0] * 11
+
+for x in data:
+    array[x] += 1
+
+result = 0
+for i in range(1, m + 1):
+    n -= array[i]
+    result += array[i]
 print(result)
