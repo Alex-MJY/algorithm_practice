9143bb54aa53973aef9716c982ae94a2420d8c7d
Alex-MJY <jmoon0227@gmail.com>
2024-01-03 23:59:06 +0900

    algorithm baekjoon 14645, 15000, 15680, 15727, 15733, 15740, 15962, 15963, 15964, 16099, 16170

--- baekjoon/14645.py
+++ baekjoon/14645.py
@@ -1,7 +1,8 @@
-stage, start = map(int, input())
+# stage, start = map(int, input())
 
-for i in range(stage):
-    a, b = map(int, input().split())
-    start += a
-    start -= b
-print(start)
+# for i in range(stage):
+#     a, b = map(int, input().split())
+#     start += a
+#     start -= b
+# print(start)
+print("비와이")
