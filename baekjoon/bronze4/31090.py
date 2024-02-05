88509fc7d7531e71eb6928df728140e105f59e22
Alex-MJY <jmoon0227@gmail.com>
2024-02-05 20:35:52 +0900

    algorithm baekjoon 30017, 30031, 30468, 30501, 30868, 30999, 31090

---
+++ baekjoon/bronze4/31090.py
@@ -0,0 +1,11 @@
+# runtime error
+
+# import sys
+
+# for i in range(int(sys.stdin.readline())):
+#     # n = int(input())
+#     n = int(sys.stdin.readline())
+#     if (n + 1) % int(str(n)[-2:-1]) == 0:
+#         print("Good")
+#     else:
+#         print("Bye")
