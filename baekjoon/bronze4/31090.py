06d20f2ce8ffa8a42c1ce4bb208feed25156f136
Alex-MJY <jmoon0227@gmail.com>
2024-03-31 11:32:50 +0900

    algorithm baekjoon 31428, 31495

--- baekjoon/bronze4/31090.py
+++ baekjoon/bronze4/31090.py
@@ -3,9 +3,18 @@
 # import sys
 
 # for i in range(int(sys.stdin.readline())):
-#     # n = int(input())
 #     n = int(sys.stdin.readline())
 #     if (n + 1) % int(str(n)[-2:-1]) == 0:
 #         print("Good")
 #     else:
 #         print("Bye")
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = str(input())
+    if (int(n) + 1) % int(n[2:]) == 0:
+        print("Good")
+    else:
+        print("Bye")
---
+++ baekjoon/bronze4/31428.py
@@ -0,0 +1,6 @@
+n = input()
+friend_arr = list(input().split())
+alice = input()
+
+
+print(friend_arr.count(alice))
