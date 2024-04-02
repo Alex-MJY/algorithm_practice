3275a9745208dfcdd6a11226b5d7f71a0841064a
Alex-MJY <jmoon0227@gmail.com>
2024-04-02 23:30:11 +0900

    algorithm baekjoon 15610, 16017, 16693, 16727

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import sys
 input = sys.stdin.readline
-x = list(map(int, input().split()))
-y = list(map(int, input().split()))
-counter = 0
 
-for i in range(len(x)):
-    if x[i] < y[i]:
-        counter += y[i] - x[i]
-        
-print(counter)
+p1, s1 = map(int, input().split())
+s2, p2 = map(int, input().split())
+
+if (p1 + p2) > (s1 + s2):
+    print("Persepolis")
+elif (p1 + p2) < (s1 + s2):
+    print("Esteghlal")
+else:
+    if s1 == p2:
+        print("Penalty")
+    elif p2 > s1:
+        print("Persepolis")
+    else:
+        print("Esteghlal")
