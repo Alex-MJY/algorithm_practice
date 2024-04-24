58008525d159dfd1a4265b34ef7d466b4bdb1d29
Alex-MJY <jmoon0227@gmail.com>
2024-04-24 23:14:02 +0900

    algorithm baekjoon 2783, 2875

--- baekjoon/bronze3/2783.py
+++ baekjoon/bronze3/2783.py
@@ -1,10 +1,10 @@
 import sys
 input = sys.stdin.readline
+arr = []
 
-x, y = map(int, input().split())
-arr = [x / y]
-for _ in range(int(input())):
-    a,b = map(int, input().split())
-    arr.append(a / b)
-
-print("%.2f" % (min(arr)*1000))
+user1 = list(map(int, input().split()))
+arr.append((1000 / user1[1]) * user1[0])
+for i in range(int(input())):
+    user2 = list(map(int, input().split()))
+    arr.append((1000 / user2[1]) * user2[0])
+print(round(min(arr), 2))
--- baekjoon/bronze3/2875.py
+++ baekjoon/bronze3/2875.py
@@ -1,17 +1,13 @@
-'''
-대회 여2 남1. n명의 여학생과 m명의 남학생이 팀원을 찾고 있다. 
-k명은 반드시 인턴쉽에 참여해야 한다. 인턴쉽에 참여하면 대회에 참여 못한다.
-만들 수 있는 최대의 수?
-'''
-
 import sys
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
+counter = 0
 
-for i in range(k):
-    if n // 2 >= m:
-        n -= 1
-    else:
-        m -= 1
-print(min(n // 2, m))
+while True:
+    n -= 2
+    m -= 1
+    if n < 0 or m < 0 or (n + m) < k:
+        break
+    counter += 1
+print(counter)
